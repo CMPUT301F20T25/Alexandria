ca3d778ef4ad10bb038bf84db50ec6a2e3e11ce0
Hankl227 <sichen3@ualberta.ca>
2020-10-20 19:15:25 -0600

    Sign up activity

--- alexandria/app/src/main/java/com/example/alexandria/MainActivity.java
+++ alexandria/app/src/main/java/com/example/alexandria/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.alexandria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Login button
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomeActivity();
+            }
+        });
+
+        // Sign Up button
+        Button signUpButton = (Button) findViewById(R.id.signUp_button);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUpActivity();
+            }
+        });
+    }
+
+    // open new activity
+    private void openHomeActivity() {
+        Intent homeIntent = new Intent(this, HomeActivity.class);
+        startActivity(homeIntent);
+    }
+
+    private void openSignUpActivity() {
+        Intent signUpIntent = new Intent(this, SignUpActivity.class);
+        startActivity(signUpIntent);
     }
 }
--- alexandria/app/src/main/java/com/example/alexandria/SignUpActivity.java
+++ alexandria/app/src/main/java/com/example/alexandria/SignUpActivity.java
@@ -2,18 +2,13 @@ package com.example.alexandria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
+        setContentView(R.layout.activity_signup);
     }
-
 }
