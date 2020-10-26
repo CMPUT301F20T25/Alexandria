c113f9eee829f7ebd78dfc515e9e85faa27b4c87
Hankl227 <sichen3@ualberta.ca>
2020-10-26 16:54:35 -0600

    Activities for My Books, Borrowed, and Requested.

--- alexandria/app/src/main/java/com/example/alexandria/HomeActivity.java
+++ alexandria/app/src/main/java/com/example/alexandria/HomeActivity.java
@@ -13,6 +13,49 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // click to view my books
+        Button myBookButton = (Button) findViewById(R.id.myBook_button);
+        myBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMyBookActivity();
+            }
+        });
+
+        // click to view borrowed books
+        Button borrowedButton = (Button) findViewById(R.id.borrowed_button);
+        borrowedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBorrowedActivity();
+            }
+        });
+
+        // click to view requested books
+        Button requestedButton = (Button) findViewById(R.id.requested_button);
+        requestedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRequestedActivity();
+            }
+        });
+    }
+
+
+    private void openMyBookActivity() {
+        Intent myBookIntent = new Intent(this, MyBookActivity.class);
+        startActivity(myBookIntent);
+    }
+
+    private void openBorrowedActivity() {
+        Intent borrowedIntent = new Intent(this, BorrowedActivity.class);
+        startActivity(borrowedIntent);
+    }
+
+    private void openRequestedActivity() {
+        Intent requestedIntent = new Intent(this, RequestedActivity.class);
+        startActivity(requestedIntent);
     }
 
     @Override
