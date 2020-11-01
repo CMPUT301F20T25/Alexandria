e895a863697162f55ef71ca6cbe86a1d9e0e3af6
xyL3 <36555005+xyL3@users.noreply.github.com>
2020-10-31 20:14:39 -0600

    Update HomeActivity.java

--- alexandria/app/src/main/java/com/example/alexandria/HomeActivity.java
+++ alexandria/app/src/main/java/com/example/alexandria/HomeActivity.java
@@ -19,8 +19,7 @@ public class HomeActivity extends BaseActivity {
         myBookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //openMyBookActivity();
-                openBook();
+                openMyBookActivity();
             }
         });
 
@@ -41,12 +40,6 @@ public class HomeActivity extends BaseActivity {
                 openRequestedActivity();
             }
         });
-    }
-
-
-    private void openBook(){
-        Intent myBookIntent = new Intent(this, BookInfoActivity.class);
-        startActivity(myBookIntent);
     }
 
 
