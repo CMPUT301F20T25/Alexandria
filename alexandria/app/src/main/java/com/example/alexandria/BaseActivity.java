a16caa6f319ab610c7802eab9bed8440c0ae9258
kay-why-ell-eh <>
2020-10-31 20:59:24 -0600

    added javadoc comments for BaseActivity class

--- alexandria/app/src/main/java/com/example/alexandria/BaseActivity.java
+++ alexandria/app/src/main/java/com/example/alexandria/BaseActivity.java
@@ -1,4 +1,7 @@
 package com.example.alexandria;
+/** Gives all subclasses a bottom navigation bar with three buttons: "Home", "Search", "My Account".
+ * @author Kyla Wong, ktwong@ualberta.ca
+ */
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -36,6 +39,11 @@ public abstract class BaseActivity extends AppCompatActivity implements BottomNa
         overridePendingTransition(0,0);
     }
 
+    /**
+     * Handles the behavior when a button on the bottom navigation bar is selected.
+     * @param item The button that was selected.
+     * @return A boolean indicating whether the selection was successful (true) or not (false).
+     */
     public boolean onNavigationItemSelected(@NonNull MenuItem item) { //remove @NotNull from params
         navigationView.postDelayed(() -> {
             int itemId = item.getItemId();
@@ -51,17 +59,33 @@ public abstract class BaseActivity extends AppCompatActivity implements BottomNa
         return true;
     }
 
+    /**
+     * Updates the status of the navigation bar (ie. which button is currently selected).
+     */
     private void updateNavigationBarState() {
         int actionId = getNavigationMenuItemId();
         selectNavigationBarItem(actionId);
     }
 
+    /**
+     * Sets the given navigation bar button to checked.
+     * @param itemId The id of the button selected on the navigation bar
+     */
     private void selectNavigationBarItem(int itemId) {
         MenuItem item = navigationView.getMenu().findItem(itemId);
         item.setChecked(true);
     }
 
+    /**
+     * Gets the content/layout file id for the activity (ex. R.layout.activity_home)
+     * @return the id of the layout file to be used.
+     */
     abstract int getContentViewId();
+
+    /**
+     * Gets the id of the bottom navigation bar for the current layout (ex. R.id.navigation)
+     * @return The id of the bottom navigation bar of the layout.
+     */
     abstract int getNavigationMenuItemId();
 
 }
