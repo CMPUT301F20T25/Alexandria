c8b3ed4ebb36ffcb851324aed96cc6cd1c5d2e99
kay-why-ell-eh <>
2020-11-20 17:07:11 -0700

    repaired broken bottom bar navigation in the Home activity

--- alexandria/app/src/main/java/com/example/alexandria/HomeActivity.java
+++ alexandria/app/src/main/java/com/example/alexandria/HomeActivity.java
@@ -33,7 +33,6 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
         Button scanButton;
         Button myBookButton;
         Button borrowedButton;
