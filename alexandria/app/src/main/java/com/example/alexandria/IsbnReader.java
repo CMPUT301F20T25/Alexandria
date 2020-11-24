7828beba465cacf51b7f6256632578bdd69b5810
kay-why-ell-eh <>
2020-11-23 21:05:18 -0700

    created initial isbn class and added ZXing package

---
+++ alexandria/app/src/main/java/com/example/alexandria/IsbnReader.java
@@ -0,0 +1,13 @@
+package com.example.alexandria;
+
+public class IsbnReader {
+    private String isbn;
+
+    public IsbnReader() {
+        //will need to connect to camera interface?
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+}
