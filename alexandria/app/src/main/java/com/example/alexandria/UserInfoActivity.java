73fd7b697357c456ce7e64874d805a72cf340938
kay-why-ell-eh <>
2020-11-29 21:06:43 -0700

    done public user profile display activity

--- alexandria/app/src/main/java/com/example/alexandria/UserInfoActivity.java
+++ alexandria/app/src/main/java/com/example/alexandria/UserInfoActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,8 +19,16 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.w3c.dom.Text;
+
 public class UserInfoActivity extends AppCompatActivity {
     private TextView username;
+    private TextView email;
+    private TextView phone;
+    private TextView bio;
+    private TextView emailDesc;
+    private TextView phoneDesc;
+    private TextView bioDesc;
     private ImageView backButton;
 
     private String userId;
@@ -40,6 +50,19 @@ public class UserInfoActivity extends AppCompatActivity {
         }
 
         username = (TextView) findViewById(R.id.userInfo_username);
+        email = (TextView) findViewById(R.id.userinfo_emailText);
+        phone = (TextView) findViewById(R.id.userinfo_phoneText);
+        bio = (TextView) findViewById(R.id.userinfo_bioText);
+
+        emailDesc = (TextView) findViewById(R.id.userinfo_emailDesc);
+        emailDesc.setTypeface(emailDesc.getTypeface(), Typeface.ITALIC);
+        emailDesc.setText("Email:");
+        phoneDesc = (TextView) findViewById(R.id.userinfo_phoneDesc);
+        phoneDesc.setTypeface(phoneDesc.getTypeface(), Typeface.ITALIC);
+        phoneDesc.setText("Phone Number:");
+        bioDesc = (TextView) findViewById(R.id.userinfo_bioDesc);
+        bioDesc.setTypeface(bioDesc.getTypeface(), Typeface.ITALIC);
+
         backButton = (ImageView) findViewById(R.id.userInfo_backImage);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,7 +71,7 @@ public class UserInfoActivity extends AppCompatActivity {
             }
         });
 
-        //setInfo();
+        setInfo();
     }
 
     private void setInfo() {
@@ -58,7 +81,13 @@ public class UserInfoActivity extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        //TODO: get and display info
+                        username.setText("@" + documentSnapshot.getData().get("username").toString());
+                        email.setText(documentSnapshot.getData().get("email").toString());
+                        String phoneNumber = documentSnapshot.getData().get("phone number").toString();
+                        phoneNumber = "(" + phoneNumber.substring(0,3) + ") " + phoneNumber.substring(4,7) + "-" + phoneNumber.substring(8);
+                        phone.setText(phoneNumber);
+                        bio.setText(documentSnapshot.getData().get("bio").toString());
+                        bioDesc.setText("A bit about @" + documentSnapshot.getData().get("username").toString() + " ...");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
